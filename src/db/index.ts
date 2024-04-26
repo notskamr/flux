c374bfa495eb632a7df59164102511a95578fa3d
Varun Sahni <sahnivarun62@gmail.com>
2024-04-26 19:06:50 +0530

    Allow using a local file instead of a Turso server

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,20 +1,29 @@
 import { drizzle } from 'drizzle-orm/libsql';
-import { Client, createClient } from '@libsql/client/web';
+import { Client, createClient } from '@libsql/client';
 import * as schema from './schema';
 
 import 'dotenv/config';
+import { migrate } from 'drizzle-orm/libsql/migrator';
 
 const client = (url: string, authToken?: string) => createClient({
     url: url,
     authToken: authToken
 });
 
-export const createDb = (client: Client) => drizzle(client, {
-    schema
-});
+export const createDb = async (client: Client) => {
+    const db = drizzle(client, {
+        schema
+    });
+    migrate(db, { migrationsFolder: "./migrations" });
+    if (client.protocol === "file" && process.env.WAL_MODE === "true") {
+        await client.execute("PRAGMA journal_mode = WAL;");
+    }
+    return db;
 
-export let db = createDb(client(process.env.TURSO_URL!, process.env.TURSO_AUTH_TOKEN!));
+};
 
-export function setDb(newDb: ReturnType<typeof createDb>) {
+export let db = await createDb(client(process.env.TURSO_URL!, process.env.TURSO_AUTH_TOKEN));
+
+export function setDb(newDb: Awaited<ReturnType<typeof createDb>>) {
     db = newDb;
 }
